f0643f2f5e7955982f609e4279bebebc886d581a
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-02-11 22:52:00 -0500

    Refactored constants out of get_reports.py
    Additional Changes
    
     * Simplified output.
     * Script now writes the URL of reports that have
       a bounty but no CVE. These reports need to be
       manually reviewed.

--- constants.py
+++ constants.py
@@ -7,3 +7,8 @@ NVD_XML_URL_TEMPLATE = 'https://nvd.nist.gov/feeds/xml/cve/{filename}'
 NVD_GZXML_FILENAME_TEMPLATE = 'nvdcve-2.0-{year}.xml.gz'
 NVD_XML_FILENAME_TEMPLATE = 'nvdcve-2.0-{year}.xml'
 NVD_CVSS_XML_NAMESPACE = 'http://scap.nist.gov/schema/cvss-v2/0.2'
+
+REPORTS_DIRECTORY = os.path.join(APP_ROOT_PATH, 'data/reports')
+HACKERONE_JSON_URL_TEMPLATE = 'https://hackerone.com/reports/{report_id}.json'
+HACKERONE_WEB_URL_TEMPLATE = 'https://hackerone.com/reports/{report_id}'
+REPORTS_REVIEW_FILEPATH = os.path.join(REPORTS_DIRECTORY, 'reports.review.txt')
--- get_reports.py
+++ get_reports.py
@@ -4,14 +4,8 @@ import os
 
 import requests
 
+from constants import *
 from logger import *
-
-JSON_URL_TEMPLATE = 'https://hackerone.com/reports/{report_id}.json'
-WEB_URL_TEMPLATE = 'https://hackerone.com/reports/{report_id}'
-REPORTS_DIRECTORY = 'reports'
-REPORT_FILENAME_TEMPLATE = REPORTS_DIRECTORY + '/{report_id}.json'
-WITH_BOUNTY_FILENAME = 'with_bounties.txt'
-WITH_CVE_FILENAME = 'with_cve.txt'
 
 
 def get_reports(min_report_id, max_report_id, num_processes):
@@ -37,7 +31,7 @@ def get_reports(min_report_id, max_report_id, num_processes):
 
 
 def get_report(report_id, sync_queue):
-    url = JSON_URL_TEMPLATE.format(report_id=report_id)
+    url = HACKERONE_JSON_URL_TEMPLATE.format(report_id=report_id)
     response = requests.get(url, allow_redirects=False)
     debug('HTTP {} for {}'.format(response.status_code, url))
     sync_queue.put(
@@ -64,41 +58,46 @@ def _save(count, sync_queue):
     with_cve = list()
     if reports:
         for (report_id, report) in reports:
-            if report:
-                filename = REPORT_FILENAME_TEMPLATE.format(report_id=report_id)
-                with open(filename, 'w') as file_:
-                    file_.write(str(report))
-                    info('{} written.'.format(filename))
-                if report['has_bounty?']:
-                    with_bounty.append(report_id)
-                if report['cve_ids']:
-                    with_cve.append(report_id)
+            filename = '{}.json'.format(report_id)
+            filepath = os.path.join(REPORTS_DIRECTORY, filename)
 
+            with open(filepath, 'w') as file_:
+                file_.write(str(report))
+            if report['has_bounty?']:
+                with_bounty.append(report_id)
+            if report['cve_ids']:
+                with_cve.append(report_id)
+
+        info('{}/{} reports publicly accessible.'.format(len(reports), count))
+        info('All publicly accessible reports downloaded to {}.'.format(
+                REPORTS_DIRECTORY
+            ))
         if with_bounty:
             info('{} reports had bounty.'.format(len(with_bounty)))
-            with open(WITH_BOUNTY_FILENAME, 'a') as file_:
-                for report_id in with_bounty:
-                    file_.write('{}\n'.format(
-                            WEB_URL_TEMPLATE.format(report_id=report_id)
-                        ))
-            info('URL of reports with bounty written to {}.'.format(
-                    WITH_BOUNTY_FILENAME
-                ))
         else:
-            warning('None of the publicly accesible reports had bounty.')
+            warning('No publicly accesible report had bounty.')
 
         if with_cve:
             info('{} reports had CVE.'.format(len(with_cve)))
-            with open(WITH_CVE_FILENAME, 'a') as file_:
-                for report_id in with_cve:
-                    file_.write('{}\n'.format(
-                            WEB_URL_TEMPLATE.format(report_id=report_id)
-                        ))
-            info('URL of reports with CVE written to {}.'.format(
-                    WITH_CVE_FILENAME
-                ))
         else:
-            warning('None of the publicly accesible reports had CVE.')
+            warning('No publicly accesible report had CVE.')
+
+        if with_bounty and with_cve:
+            bounty_wo_cve = set(with_bounty) - set(with_cve)
+            if bounty_wo_cve:
+                with open(REPORTS_REVIEW_FILEPATH, 'a') as file_:
+                    for report_id in bounty_wo_cve:
+                        file_.write('{}\n'.format(
+                                HACKERONE_WEB_URL_TEMPLATE.format(
+                                    report_id=report_id
+                                )
+                            ))
+                info(
+                        (
+                            'URL of reports that had bounty without CVE '
+                            'written to {}. There were {} such reports.'
+                        ).format(REPORTS_REVIEW_FILEPATH,  len(bounty_wo_cve))
+                    )
     else:
         warning('No reports in the range specified were publicly accessible.')
 
