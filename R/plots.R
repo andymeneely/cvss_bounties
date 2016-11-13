2565a774ae2ff439a7635b146531802da509d605
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-11-13 18:42:01 -0500

    Add hexgonal bin ploting for scatter plots
    Additional Refactor
    
      * Color palettes
      * Plot to associate qualitative code with products
      * Bounty is now Vulnerability Bounty

--- R/plots.R
+++ R/plots.R
@@ -1,4 +1,4 @@
-# Clear
+  # Clear
 rm(list = ls())
 cat("\014")
 
@@ -9,7 +9,7 @@ source("library.R")
 init.libraries()
 
 # Database Connection
-db.connection <- get.db.connection()
+db.connection <- get.db.connection(environment="PRODUCTION")
 dataset <- db.get.data(db.connection, QUERY)
 db.disconnect(db.connection)
 
@@ -19,102 +19,155 @@ dataset <- transform.dataset(dataset)
 # Plots
 
 ## Theme
-plot.theme <-
-  theme_bw() +
-  theme(
-    plot.title = element_text(size = 14, face = "bold", margin = margin(5,0,15,0)),
-    axis.text.x = element_text(size=10, colour = "black", angle = 50, vjust = 1, hjust = 1),
-    axis.title.x = element_text(colour = "black", face = "bold", margin = margin(15,0,5,0)),
-    axis.text.y = element_text(size=10, colour = "black"),
-    axis.title.y = element_text(colour = "black", face = "bold", margin = margin(0,15,0,5)),
-    strip.text.x = element_text(size=10, face="bold")
+use.color <- F
+if(use.color){
+  plot.trendline <- "gray45"
+  # Green Palette
+  plot.palette <- c(
+    "#f2f7ee","#e5f0dd","#d8e8cc","#cbe1bc","#bed9ab","#b1d29a","#a4ca89",
+    "#97c379","#8abb68","#7db457","#70ad47","#669e41","#5c8e3b","#527e34",
+    "#486f2e","#3e5f27","#334f21","#293f1a","#1f3014","#15200d","#  0b1007"
   )
+  values.palette <- c(
+    "LOCAL" = "#d8e8cc", "ADJACENT_NETWORK" = "#97c379", "NETWORK" = "#5c8e3b",
+    "SINGLE_INSTANCE" = "#97c379", "MULTIPLE_INSTANCES" = "#5c8e3b",
+    "LOW" = "#d8e8cc", "MEDIUM" = "#97c379", "HIGH" = "#5c8e3b",
+    "NONE" = "#d8e8cc", "PARTIAL" = "#97c379", "COMPLETE" = "#5c8e3b"
+  )
+  response.palette <- c("score" = "#f1a069", "amount" = "#97c379")
+  hilo.palette <- c("0" = "#ffffff", "1" = "#000000")
+} else {
+  plot.trendline <- "#000000"
+  plot.palette <- c(
+    "#f9f9f9","#f3f3f3","#ededed","#e7e7e7","#e1e1e1","#dcdcdc","#d6d6d6",
+    "#d0d0d0","#cacaca","#c4c4c4","#bfbfbf","#aeaeae","#9d9d9d","#8b8b8b",
+    "#7a7a7a","#696969","#575757","#464646","#353535","#232323","#121212"
+  )
+  values.palette <- c(
+    "LOCAL" = "#ededed", "ADJACENT_NETWORK" = "#d0d0d0", "NETWORK" = "#9d9d9d",
+    "SINGLE_INSTANCE" = "#d0d0d0", "MULTIPLE_INSTANCES" = "#9d9d9d",
+    "LOW" = "#ededed", "MEDIUM" = "#d0d0d0", "HIGH" = "#9d9d9d",
+    "NONE" = "#ededed", "PARTIAL" = "#d0d0d0", "COMPLETE" = "#9d9d9d"
+  )
+  response.palette <- c("score" = "#e7e7e7", "amount" = "#bfbfbf")
+  hilo.palette <- c("0" = "#ffffff", "1" = "#000000")
+}
 
-blank.theme <- plot.theme +
-  theme(
-    panel.border = element_rect(colour = NA),
-    panel.grid.minor = element_blank(),
-    panel.grid.major = element_blank(),
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    axis.ticks =  element_blank(),
-    plot.margin = unit(c(0,0,0,0), "lines")
-  )
+## Boxplots
+# Export Resolution: 600 x 150
+ggplot(dataset %>% mutate(metric = "bounty"), aes(x = metric, y = amount)) +
+  geom_boxplot() +
+  scale_y_log10(labels=scales::dollar) +
+  labs(
+    title = "Distribution of Vulnerability Bounty (Log Scale)",
+    x = NULL, y = NULL
+  ) +
+  get.theme() +
+  theme(axis.text.y = element_blank()) +
+  coord_flip()
 
 ## Boxplots by Product
 product.labels <- table(dataset$product)
 for(i in 1:length(product.labels)){
-  product.labels[i] <- paste(names(product.labels)[i], " (", product.labels[i], ")", sep = "")
+  product.labels[i] <- paste(
+    names(product.labels)[i], " (", product.labels[i], ")", sep = ""
+  )
 }
 
-qplot(
-  product, amount, data = dataset,
-  main = "",
-  xlab = "Product (Number of Vulnerabilities)", ylab = "Bounty (Log Scale)", geom = "boxplot"
-) +
-  plot.theme +
+ggplot(dataset, aes(x = product, y = amount)) +
+  geom_boxplot() +
+  labs(
+    x = "Product (Number of Vulnerabilities)",
+    y = "Vulerability Bounty (Log Scale)"
+  ) +
   scale_y_log10(labels = scales::dollar) +
-  scale_x_discrete(
-    breaks = names(product.labels), labels = product.labels
-  )
+  scale_x_discrete(breaks = names(product.labels), labels = product.labels) +
+  get.theme()
 
 ## Scatterplots
 
-### Entire Dataset
-data.source <- dataset
+### Base Score Only
+plot.source <- dataset %>%
+  select("amount" = amount, "score" = score) %>%
+  mutate("type" = "Base Score")
 
-#### Base Score Only
-plot.source <- rbind(
-  data.frame(
-    "type" = "Base Score",
-    "amount" = data.source$amount, "score" = data.source$score
-  )
-)
+correlation <- get.spearmansrho(dataset, "amount", "score")
 plot.labels <- data.frame(
   "type" = c("Base Score"),
-  "x" = c(min(data.source$score)),
-  "y" = c(max(data.source$amount)),
-  "correlation" = c(
-    get.spearmansrho(data.source, "amount", "score")
-  )
+  "x" = c(min(dataset$score)), "y" = c(max(dataset$amount)),
+  "correlation" = c(correlation$rho)
 )
 
+##### Simple Scatter Plot
 ggplot(plot.source, aes(x = score, y = amount)) +
   geom_point() +
-  geom_smooth(method = "lm", colour = "gray45", linetype = "dashed") +
-  facet_grid(. ~ type, scales = "free", space = "free") +
+  geom_smooth(method = "lm", colour = plot.trendline, linetype = "dashed") +
   scale_x_continuous(breaks = seq(0.0, 10.0, by = 0.5)) +
   scale_y_continuous(labels = scales::dollar) +
   geom_text(
     data = plot.labels, parse = T, inherit.aes = F, hjust = 0, vjust = 1,
     aes(x = x, y = y, label = paste("rho==", correlation, sep = ""))
   ) +
-  labs(title = "", x = "Score", y = "Bounty") +
-  plot.theme
+  labs(x = "CVSS Base Score", y = "Bounty") +
+  get.theme()
+
+##### Hexagonal Scatter Plot
+# Export Resolution: 600 x 350
+ggplot(plot.source, aes(x = score, y = amount)) +
+  geom_hex(bins = 30, colour = "black") +
+  geom_smooth(method = "lm", colour = plot.trendline, linetype = "dashed") +
+  geom_text(
+    data = plot.labels, parse = T, inherit.aes = F, hjust = 0, vjust = 1,
+    aes(x = x, y = y, label = paste("rho==", correlation, sep = ""))
+  ) +
+  scale_fill_gradientn(colors = plot.palette, name = "Density") +
+  scale_x_continuous(breaks = seq(0.0, 10.0, by = 0.5)) +
+  scale_y_continuous(labels = scales::dollar) +
+  guides(fill = guide_colorbar(
+    barwidth = 0.5, barheight = 3, ticks = F,
+    title.theme = element_text(size = 10, angle = 0, face = "bold")
+  )) +
+  labs(x = "CVSS Base Score", y = "Vulnerability Bounty") +
+  get.theme()
 
 #### Subscores Only
-plot.source <- rbind(
-  data.frame(
-    "type" = "Exploitability Subscore",
-    "amount" = data.source$amount, "score" = data.source$exploitability_subscore
-  ),
-  data.frame(
-    "type" = "Impact Subscore",
-    "amount" = data.source$amount, "score" = data.source$impact_subscore
-  )
+plot.source <- bind_rows(
+  dataset %>%
+    select("amount" = amount, "score" = exploitability_subscore) %>%
+    mutate("type" = "CVSS Exploitability Subscore"),
+  dataset %>%
+    select("amount" = amount, "score" = impact_subscore) %>%
+    mutate("type" = "CVSS Impact Subscore")
 )
-plot.labels <- data.frame(
-  "type" = c("Exploitability Subscore", "Impact Subscore"),
-  "x" = c(
-    min(data.source$exploitability_subscore, data.source$impact_subscore),
-    min(data.source$exploitability_subscore, data.source$impact_subscore)
-  ),
-  "y" = c(max(data.source$amount), max(data.source$amount)),
-  "correlation" = c(
-    get.spearmansrho(data.source, "amount", "exploitability_subscore"),
-    get.spearmansrho(data.source, "amount", "impact_subscore")
-  )
+
+plot.labels <- data.frame()
+x <- c(
+  min(dataset$exploitability_subscore, dataset$impact_subscore),
+  min(dataset$exploitability_subscore, dataset$impact_subscore)
 )
+y <- c(max(dataset$amount), max(dataset$amount))
+correlation <- get.spearmansrho(dataset, "amount", "exploitability_subscore")
+if(correlation$significant){
+  plot.labels <- rbind(
+    plot.labels,
+    data.frame(
+      "type" = "CVSS Exploitability Subscore", "x" = x, "y" = y,
+      "correlation" = correlation$rho
+    )
+  )
+}
+correlation <- get.spearmansrho(dataset, "amount", "impact_subscore")
+if(correlation$significant){
+  plot.labels <- rbind(
+    plot.labels,
+    data.frame(
+      "type" = "CVSS Impact Subscore", "x" = x, "y" = y,
+      "correlation" = correlation$rho
+    )
+  )
+}
+
+##### Simple Scatter Plot
 ggplot(plot.source, aes(x = score, y = amount)) +
   geom_point() +
   geom_smooth(method = "lm", colour = "gray45", linetype = "dashed") +
@@ -122,134 +175,143 @@ ggplot(plot.source, aes(x = score, y = amount)) +
   scale_x_continuous(breaks = seq(0.0, 10.0, by = 0.5)) +
   scale_y_continuous(labels = scales::dollar) +
   geom_text(
-    data = plot.labels, parse = T, inherit.aes = F,
-    aes(x = x, y = y, label = paste("rho==", correlation, sep = ""), hjust = 0, vjust = 1)
+    data = plot.labels, parse = T, inherit.aes = F, hjust = 0, vjust = 1,
+    aes(x = x, y = y, label = paste("rho==", correlation, sep = ""))
   ) +
-  labs(title = "", x = "Score", y = "Bounty") +
-  plot.theme
+  labs(x = "Score", y = "Vulnerability Bounty") +
+  get.theme()
+
+##### Hexagonal Scatter Plot
+# Export Resolution: 1000 x 350
+ggplot(plot.source, aes(x = score, y = amount)) +
+  geom_smooth(method = "lm", colour = plot.trendline, linetype = "dashed") +
+  geom_hex(bins = 30, colour = "black") +
+  geom_text(
+    data = plot.labels, parse = T, inherit.aes = F, hjust = 0, vjust = 1,
+    aes(x = x, y = y, label = paste("rho==", correlation, sep = ""))
+  ) +
+  scale_fill_gradientn(colours = plot.palette, name = "Density") +
+  scale_x_continuous(breaks = seq(0.0, 10.0, by = 0.5)) +
+  scale_y_continuous(labels = scales::dollar) +
+  guides(fill = guide_colorbar(
+    barwidth = 0.5, barheight = 3, ticks = F,
+    title.theme = element_text(size = 10, angle = 0, face = "bold")
+  )) +
+  facet_grid(. ~ type, scales = "free", space = "free") +
+  labs(x = "Subscore", y = "Vulnerability Bounty") +
+  get.theme()
 
 ## Boxplots
 
 ### Boxplot of Bounty by CVSS Metric
-data.source <- dataset
 plot.source <- rbind(
   data.frame(
     "metric" = "access_complexity", "label" = "Access Complexity",
-    "value" = data.source$access_complexity, "amount" = data.source$amount
+    "value" = dataset$access_complexity, "amount" = dataset$amount
   ),
   data.frame(
     "metric" = "access_vector", "label" = "Access Vector",
-    "value" = data.source$access_vector, "amount" = data.source$amount
+    "value" = dataset$access_vector, "amount" = dataset$amount
   ),
   data.frame(
     "metric" = "authentication", "label" = "Authentication",
-    "value" = data.source$authentication, "amount" = data.source$amount
+    "value" = dataset$authentication, "amount" = dataset$amount
   ),
   data.frame(
     "metric" = "availability_impact", "label" = "Availability Impact",
-    "value" = data.source$availability_impact, "amount" = data.source$amount
+    "value" = dataset$availability_impact, "amount" = dataset$amount
   ),
   data.frame(
     "metric" = "confidentiality_impact", "label" = "Confidentiality Impact",
-    "value" = data.source$confidentiality_impact, "amount" = data.source$amount
+    "value" = dataset$confidentiality_impact, "amount" = dataset$amount
   ),
   data.frame(
     "metric" = "integrity_impact", "label" = "Integrity Impact",
-    "value" = data.source$integrity_impact, "amount" = data.source$amount
+    "value" = dataset$integrity_impact, "amount" = dataset$amount
   )
 )
 
-ggplot(plot.source, aes(x = value, y = amount)) +
+#### Single Row
+# Export Resolution: 1200 x 350
+ggplot(plot.source, aes(x = value, y = amount, fill = value)) +
   geom_boxplot() +
   facet_wrap(~ label, scales = "free_x", nrow = 1) +
   scale_x_discrete(breaks = waiver(), labels = METRIC.VALUE.LABELS) +
   scale_y_log10(labels = scales::dollar) +
-  labs(title = "", x = "Metric Value", y = "Bounty (Log Scale)") +
-  plot.theme
+  scale_fill_manual(values = values.palette) +
+  labs(x = "Metric Value", y = "Vulnerability Bounty (Log Scale)") +
+  get.theme() +
+  theme(legend.position = "none")
 
-## Histograms
-title = "Distribution of Bounty for Vulnerabilities with metric_value metric_label"
-for(metric in names(METRIC.LABELS)){
-  cat(METRIC.LABELS[[metric]],"\n")
-  for(value in METRIC.VALUES[[metric]]){
-    cat("  ", value, "\n")
-    filtered.dataset <- dataset[dataset[[metric]] == value,]
-    if(nrow(filtered.dataset) > 0){
-      # Boxplot Statistics
-      boxplot.statistics <- boxplot.stats(filtered.dataset$amount)$stats
+#### Double Row
+# Export Resolution:
+ggplot(plot.source, aes(x = value, y = amount, fill = value)) +
+  geom_boxplot() +
+  facet_wrap(~ label, scales = "free_x", nrow = 2) +
+  scale_x_discrete(breaks = waiver(), labels = METRIC.VALUE.LABELS) +
+  scale_y_log10(labels = scales::dollar) +
+  scale_fill_manual(values = values.palette) +
+  labs(x = "Metric Value", y = "Vulnerability Bounty (Log Scale)") +
+  get.theme() +
+  theme(legend.position = "none")
 
-      # Number of Outliers
-      outliers.count <- length(which(filtered.dataset$amount > outlier.cutoff))
+## Associating Qualitative Codes to Products
+associations <- read.csv(
+  "codeassociation.csv", header = T, stringsAsFactors = F
+) %>% filter(., code != 'The Internet')
 
-      # Remove Outliers (For Plotting Purposes Only)
-      if(outliers.count > 0){
-        filtered.dataset <- filtered.dataset[filtered.dataset$amount <= outlier.cutoff,]
-      }
+# products <- sort(unique(associations$product))
+products <- (associations %>%
+               group_by(., product) %>%
+               summarize(., count = n()) %>%
+               arrange(-count) %>%
+               select(product))$product
+# codes <- sort(unique(associations$code))
+codes <- (associations %>%
+            group_by(., code) %>%
+            summarize(., count = n()) %>%
+            arrange(-count) %>%
+            select(code))$code
+association.matrix <- matrix(
+  data = 0, nrow = length(codes), ncol = length(products)
+)
+rownames(association.matrix) <- codes
+colnames(association.matrix) <- products
 
-      # Boxplot
-
-      ## Plot the Statistics
-      plot.box <- ggplot(
-        filtered.dataset, aes(
-            x = factor(value), y = amount,
-            ymin = boxplot.statistics[1], lower = boxplot.statistics[2],
-            middle = boxplot.statistics[3],
-            upper = boxplot.statistics[4], ymax = boxplot.statistics[5]
-          )
-        ) + geom_boxplot(stat = "identity")
-      ## Plot the Outliers in the Filtered Dataset
-      filtered.dataset.outliers <- filtered.dataset[filtered.dataset$amount > boxplot.statistics[5],]
-      if(nrow(filtered.dataset.outliers) > 0){
-        plot.box <- plot.box +
-          geom_point(aes(y = amount), data = filtered.dataset.outliers)
-      }
-      ## Indicate Number of Remaining Outliers
-      plot.box <- plot.box +
-        scale_y_continuous(limits = c(0, max(filtered.dataset$amount) + 1000)) +
-        geom_text(aes(
-          y = max(filtered.dataset$amount) + 1000,
-          label = paste("(", outliers.count, ")", sep = "")
-        ))
-      ## Add Titles
-      plot.box <- plot.box +
-        labs(
-          list(title = str_replace(
-            str_replace(
-              title, "metric_value", METRIC.VALUE.LABELS[[value]]
-            ), "metric_label", METRIC.LABELS[[metric]]
-          ))
-        )
-      ## Flip Coordinates and Assign Theme
-      plot.box <- plot.box + coord_flip() + blank.theme
-
-      # Histogram
-      plot.hist <- qplot(
-        amount, data = filtered.dataset,
-        xlab = "Bounty", ylab = "Frequency",
-        geom = "histogram", bins = 50
-      ) +
-        plot.theme +
-        theme(axis.text.x = element_text(angle = 50, vjust = 1, hjust = 1)) +
-        scale_x_continuous(
-          labels = scales::dollar, expand = c(0,0),
-          limits = c(0, max(filtered.dataset$amount) + 1000)
-        )
-
-      # Layout Plots in a Table
-      plot.box <- ggplot_gtable(ggplot_build(plot.box))
-      plot.hist <- ggplot_gtable(ggplot_build(plot.hist))
-      # Maximum Width
-      max.width <- unit.pmax(plot.box$widths[2:3], plot.hist$widths[2:3])
-
-      plot.box$widths[2:3] <- as.list(max.width)
-      plot.hist$widths[2:3] <- as.list(max.width)
-
-      gt <- gtable(widths = unit(c(5), "null"), height = unit(c(1, 5), "null"))
-      gt <- gtable_add_grob(gt, plot.box, 1, 1)
-      gt <- gtable_add_grob(gt, plot.hist, 2, 1)
-
-      grid.newpage()
-      grid.draw(gt)
+for(p in products){
+  cat(p, "\n")
+  codes.subset <- associations %>% filter(., product == p)
+  for(c in codes){
+    if(c %in% codes.subset$code){
+      association.matrix[c, p] <- 1
     }
   }
 }
+
+groups <- read.csv("groups.csv", header = TRUE)
+groups$code <- factor(groups$code, levels = codes)
+
+plot.source <- melt(association.matrix) %>%
+  select(code = Var1, product = Var2, value)
+
+plot.source$code <- factor(plot.source$code, levels = codes)
+plot.source$product <- factor(plot.source$product, levels = products)
+plot.source$value <- factor(plot.source$value, levels = c(0,1))
+plot.source <- inner_join(plot.source, groups, by = "code")
+plot.source$group <- factor(
+  plot.source$group, levels = c("generic", "technology", "product", "quality")
+)
+
+# Export Resolution: 1000 x 600
+ggplot(data = plot.source, aes(x = code, y = product, fill = value)) +
+  geom_tile(color = "#e7e7e7", size = 1) +
+  scale_fill_manual(values = hilo.palette) +
+  # scale_x_discrete(label = format.label) +
+  coord_fixed() +
+  facet_grid(
+    . ~ group, scales = "free_x", space = "free",
+    labeller = as_labeller(QCODE.GROUP.LABELS)
+  ) +
+  labs(x = "Vulnerability Bounty Determination Criterion", y = "Product") +
+  get.theme() +
+  theme(legend.position = "none")
